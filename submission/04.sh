c24f577f27d04941ed4dc16d3e01baf875422318
csserumaga <alltechissues@gmail.com>
2026-07-19 17:02:13 +0300

    added eigth file

--- submission/04.sh
+++ submission/04.sh
@@ -1,6 +1,7 @@
 # Create a CLTV script with a timestamp of 1495584032 and public key below:
 publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 
-script="0420cd2459b17521${publicKey}ac"
+
+script="0420cd2459b17576a914${pubKeyHash}88ac"
 
 echo "$script"
