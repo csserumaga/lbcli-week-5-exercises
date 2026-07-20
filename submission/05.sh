6b204ef9c7c378bb12ef1521e60f9dee8555143f
csserumaga <alltechissues@gmail.com>
2026-07-20 19:58:17 +0300

    Fixed file 5

--- submission/05.sh
+++ submission/05.sh
@@ -1,5 +1,6 @@
 # Create a CSV script that would lock funds until one hundred and fifty blocks had passed
-publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
-script="029600b27521${publicKey}ac"
+pubKeyHash="1e51fcdc14be9a148bb0aaec9197eb47c83776fb"
+
+script="029600b27576a914${pubKeyHash}88ac"
 
 echo "$script"
